cd4c0ebce5ef06b6bdfe99f49c2ffdc6b435371d
twossu <nadao202@naver.com>
2026-03-24 22:14:51 +0900

    feat: 투두리스트 ui완료

--- twossu/chapter2/src/App.tsx
+++ twossu/chapter2/src/App.tsx
@@ -1,20 +1,34 @@
+import "./App.css";
 import Button from "./components/Button/Button";
 
 function App() {
   return (
-    <>
+    <div className="container">
       <h1>TWOSSU TODO</h1>
-      <input placeholder="할 일 입력" />
-      <Button mode="add" onClick={() => {}}>
-        추가
-      </Button>{" "}
-      <Button mode="delete" onClick={() => {}}>
-        삭제
-      </Button>{" "}
-      <Button mode="complete" onClick={() => {}}>
-        완료
-      </Button>
-    </>
+
+      <div className="input-section">
+        <input className="input" placeholder="할 일 입력" />
+        <Button mode="add" onClick={() => {}}>
+          추가
+        </Button>
+      </div>
+
+      <div className="todo-section">
+        <div className="todo">
+          <h2>할 일</h2>
+          <Button mode="complete" onClick={() => {}}>
+            완료
+          </Button>
+        </div>
+
+        <div className="todo">
+          <h2>완료</h2>
+          <Button mode="delete" onClick={() => {}}>
+            삭제
+          </Button>
+        </div>
+      </div>
+    </div>
   );
 }
 
