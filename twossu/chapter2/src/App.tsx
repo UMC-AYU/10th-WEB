d1b077d6595e19e06d46f021be7065c97a136ce7
twossu <nadao202@naver.com>
2026-03-24 21:51:47 +0900

    feat: 버튼 컴포넌트 구현

--- twossu/chapter2/src/App.tsx
+++ twossu/chapter2/src/App.tsx
@@ -1,9 +1,19 @@
-import "./App.css";
+import Button from "./components/Button/Button";
 
 function App() {
   return (
     <>
-      <h1>Hello React</h1>
+      <h1>TWOSSU TODO</h1>
+      <input placeholder="할 일 입력" />
+      <Button mode="add" onClick={() => {}}>
+        추가
+      </Button>{" "}
+      <Button mode="delete" onClick={() => {}}>
+        삭제
+      </Button>{" "}
+      <Button mode="complete" onClick={() => {}}>
+        완료
+      </Button>
     </>
   );
 }
