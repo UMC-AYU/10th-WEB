2d6960d88330df0f1ada3349b1fc9e8b71a85e83
twossu <nadao202@naver.com>
2026-03-31 15:30:03 +0900

    feat: 영화 불러오기 기초

--- twossu/chapter3/mission1/src/pages/movies.tsx
+++ twossu/chapter3/mission1/src/pages/movies.tsx
@@ -1,12 +1,38 @@
-// src/pages/movies.tsx
-import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import type { Movie, MovieResponse } from "../types/movie";
+import axios from "axios";
 
 const MoviesPage = () => {
-  const params = useParams(); // { movieId?: string }
+  const token = import.meta.env.VITE_TMDB_TOKEN;
+  const [movies, setMovies] = useState<Movie[]>([]);
 
-  console.log(params); // 예: { movieId: "123" }
+  useEffect(() => {
+    const fetchMovies = async () => {
+      const { data } = await axios.get<MovieResponse>(
+        "https://api.themoviedb.org/3/movie/popular?language=ko-KR&page=1",
+        {
+          headers: {
+            Authorization: `Bearer ${token}`, // 본인 TMDB 토큰으로 교체
+          },
+        },
+      );
+      setMovies(data.results);
+    };
 
-  return <h1>{params.movieId}번의 Movies Page 야호~!</h1>;
+    fetchMovies();
+  }, []);
+
+  console.log(movies);
+
+  return (
+    <ul>
+      {movies?.map((movie) => (
+        <li key={movie.id}>
+          <h1>{movie.title}</h1>
+        </li>
+      ))}
+    </ul>
+  );
 };
 
 export default MoviesPage;
