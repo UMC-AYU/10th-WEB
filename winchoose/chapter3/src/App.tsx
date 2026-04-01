1e787abd38c739ef120202a979605f8f287cb645
winchoose <tmdxor741@naver.com>
2026-04-01 10:04:49 +0900

    feature: chapter3 mission1

--- winchoose/chapter3/src/App.tsx
+++ winchoose/chapter3/src/App.tsx
@@ -1,35 +1,31 @@
 import "./App.css";
-import { Route } from "./Route";
-import { Link, Routes } from "./Router";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-const Header = () => {
-  return (
-    <nav style={{ display: "flex", gap: "10px" }}>
-      <Link to="/taek">TAEK</Link>
-      <Link to="/aeong">AEONG</Link>
-      <Link to="/joy">JOY</Link>
-      <Link to="/not-found">NOT FOUND</Link>
-    </nav>
-  );
-};
+import HomePage from "./pages/home";
+import NotFound from "./pages/not-found";
+import Movies from "./pages/movies";
+import RootLayout from "./layout/root-layout";
 
-const MatthewPage = () => <h1>택이 페이지</h1>;
-const AeongPage = () => <h1>애옹 페이지</h1>;
-const JoyPage = () => <h1>조이 페이지</h1>;
-const NotFoundPage = () => <h1>Not Found 페이지</h1>;
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: "movies",
+        element: <Movies />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <>
-      <Header />
-      <Routes>
-        <Route path="/taek" component={MatthewPage} />
-        <Route path="/aeong" component={AeongPage} />
-        <Route path="/joy" component={JoyPage} />
-        <Route path="/not-found" component={NotFoundPage} />
-      </Routes>
-    </>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
