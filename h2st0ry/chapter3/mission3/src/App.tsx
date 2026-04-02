e18e020eb9ac968f2bbb46653ffaf13c6c65bca8
h2st0ry <yeonhee040512@gmail.com>
2026-04-02 12:07:49 +0900

    router 분리

--- h2st0ry/chapter3/mission3/src/App.tsx
+++ h2st0ry/chapter3/mission3/src/App.tsx
@@ -1,41 +1,6 @@
 import "./App.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import HomePage from "./pages/Home";
-import NotFound from "./pages/NotFound";
-import RootLayout from "./layout/root-layout";
-import MoviesPage from "./pages/Movies";
-import MovieDetailPage from "./pages/Movie-Detail";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <RootLayout />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: "movies/:category",
-        element: <MoviesPage />,
-      },
-      {
-        path: "movies/detail/:movieId",
-        element: <MovieDetailPage />,
-      },
-      {
-        path: "not-found",
-        element: <NotFound />,
-      },
-      {
-        path: "*",
-        element: <NotFound />,
-      },
-    ],
-  },
-]);
+import { RouterProvider } from "react-router-dom";
+import { router } from "./routes/router";
 
 function App() {
   return <RouterProvider router={router} />;
---
+++ h2st0ry/chapter3/mission3/src/api/movie.ts
@@ -0,0 +1,5 @@
+import axios from "axios";
+
+export const getMovies = (category: string, page: number) => {
+  return axios.get(...);
+};
